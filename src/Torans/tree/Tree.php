5501e7f2e318938673799e9139c29aea947c9a86
SHETU <demo.ran@icloud.com>
2018-04-04 13:16:55 +0800

    change function name

--- src/Torans/tree/Tree.php
+++ src/Torans/tree/Tree.php
@@ -11,26 +11,26 @@
 namespace Torans\Tree;
 
 
-class ArrayToTree
+class Tree
 {
 
     /**
      * 数组层级缩进转换
      * @param array $array 源数组
-     * @param int   $pid
-     * @param int   $level
+     * @param int $pid
+     * @param int $level
      * @return array
      */
-    function arr2level($array, $pid = 0, $level = 1)
+    static function arr2level($array, $pid = 0, $level = 1)
     {
         static $list = [];
         $prefix = '|-';
         foreach ($array as $v) {
             if ($v['pid'] == $pid) {
                 $v['level'] = $level;
-                $v['_name'] = $prefix.$v['name'];
-                $list[]     = $v;
-                $this->array2level($array, $v['id'], $level + 1);
+                $v['_name'] = $prefix . $v['name'];
+                $list[] = $v;
+                self::arr2level($array, $v['id'], $level + 1);
             }
         }
         return $list;
@@ -43,14 +43,15 @@ class ArrayToTree
      * @param int $level
      * @return array
      */
-    function arr2tree($tree, $rootId = 0,$level=1) {
+    static function arr2tree($tree, $rootId = 0, $level = 1)
+    {
         $return = array();
-        foreach($tree as $leaf) {
-            if($leaf['pid'] == $rootId) {
+        foreach ($tree as $leaf) {
+            if ($leaf['pid'] == $rootId) {
                 $leaf["level"] = $level;
-                foreach($tree as $subleaf) {
-                    if($subleaf['pid'] == $leaf['id']) {
-                        $leaf['children'] = $this->arr2tree($tree, $leaf['id'],$level+1);
+                foreach ($tree as $subLeaf) {
+                    if ($subLeaf['pid'] == $leaf['id']) {
+                        $leaf['children'] = self::arr2tree($tree, $leaf['id'], $level + 1);
                         break;
                     }
                 }
