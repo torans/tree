d885e7773f282b7846ec9a0bef7e88b45a5e5561
SHETU <demo.ran@icloud.com>
2018-04-04 11:11:04 +0800

    Add test file

---
+++ examples/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 类&文件说明
+ * Created by PhpStorm.
+ * User: Demo.Ran
+ * Date: 2018/4/4
+ * Time: 10:55
+ * Email: demo.ran@icloud.com
+ */
+
+class Test
+{
+
+    public function toLevel()
+    {
+        $data = [];
+        return array2level($data);
+    }
+}
